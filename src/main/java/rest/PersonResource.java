55e1d96b8cc3f68d4720b8b04e9de3995b7c0c16
Frederik Hess <cph-fp103@cphbusiness.dk>
2022-09-12 14:15:13 +0200

    initial changes into my personal settings

---
+++ src/main/java/rest/PersonResource.java
@@ -0,0 +1,36 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dtos.RenameMeDTO;
+import utils.EMF_Creator;
+import facades.PersonFacade;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+//Todo Remove or change relevant parts before ACTUAL use
+@Path("person")
+public class PersonResource {
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+       
+    private static final PersonFacade FACADE =  PersonFacade.getPersonFacade(EMF);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+            
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String demo() {
+        return "{\"msg\":\"Hello Frederik :-)\"}";
+    }
+
+//    @POST
+//    @Produces({MediaType.APPLICATION_JSON})
+//    @Consumes({MediaType.APPLICATION_JSON})
+//    public Response postExample(String input){
+////        RenameMeDTO rmdto = GSON.fromJson(input, RenameMeDTO.class);
+////        System.out.println(rmdto);
+//        return Response.ok().entity().build();
+//    }
+}
